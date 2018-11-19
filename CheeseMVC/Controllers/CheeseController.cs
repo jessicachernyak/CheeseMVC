e75333ee0dab48d654cd5a33f01ecc46ca6b00d7
Jessica Chernyak <jessicachernyak@gmail.com>
2018-11-18 21:57:39 -0600

    * CheeseController.cs: changed my dictionary to a list
    * Cheese.cs:
    * Index.cshtml:

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,12 +13,13 @@ namespace CheeseMVC.Controllers
     {
 
         // static private List<string> Cheeses = new List<string>();
-        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+        // static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+        static private List<Cheese> cheeses = new List<Cheese>();
 
         // GET: /<controller>/
         public IActionResult Index()
         {
-           ViewBag.cheeses = Cheeses;
+            ViewBag.cheeses = cheeses;
 
             return View();
         }
@@ -32,18 +34,17 @@ namespace CheeseMVC.Controllers
         [Route("/Cheese/Add")]
         public IActionResult NewCheese(string name, string description)
         {
-            
-            Cheeses.Add(name, description);
+            Cheese cheese = new Cheese(name, description);
+            cheeses.Add(cheese);
+
             return Redirect("/Cheese");
         }
 
         [HttpPost]
-        public IActionResult RemoveCheese(string[] rmvcheese)
+        public IActionResult RemoveCheese(List<string> rmvcheese)
         {
-            foreach(string cheese in rmvcheese)
-            {
-                Cheeses.Remove(cheese);
-            }
+            cheeses.RemoveAll(cheese => rmvcheese.Contains(cheese.name));
+
             return Redirect("/Cheese");
         }
     }
---
+++ CheeseMVC/Models/Cheese.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CheeseMVC.Models
+{
+    public class Cheese
+    {
+        public string name;
+        public string description;
+
+        public Cheese(string name, string description) {
+            this.name = name;
+            this.description = description;
+        }
+    }
+}
